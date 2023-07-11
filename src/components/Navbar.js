4a241c958dd191de060c6a42ea03c3e986fdd056
Roei <r.balisha@gmail.com>
2023-07-11 08:27:02 +0100

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import '../CSS/Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <span>Home</span>
+            <span>Reserch</span>
+            <span>Publications</span>
+            <span>Lab Members</span>
+            <span>Contact</span>
+        </div>
+    )
+}
+
+export default Navbar;
