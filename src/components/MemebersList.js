01e8bfbee76df0c310c31df71cab37fefbd13615
Roei-Be <r.balisha@gmail.com>
2023-08-01 16:45:04 +0300

    started working on dark mode

--- src/components/MemebersList.js
+++ src/components/MemebersList.js
@@ -68,7 +68,8 @@ const MembersList = () => {
                             <div className="memberItem">
                                 <div className='img'><img src={`${member.Photo ? member.Photo : "https://static.wixstatic.com/media/56112d_1efe4d20db6249f1a5876256376aabbc~mv2.gif"}`} alt="" /></div>
                                 <div className="content">
-                                    <span className='name'>{member.Name}<p>&nbsp;{member.Start_Year} - {member.End_Year}</p></span>
+                                    {/* <span className='name'>{member.Name}<p>&nbsp;{` (${member.Start_Year} - ${member.End_Year})`}</p></span> */}
+                                    <span className='name'>{`${member.Name} (${member.Start_Year} - ${member.End_Year})`}</span>
                                     <span className='info'>
                                         {`${member.Role} ● `} 
                                         <div className="socials">
@@ -80,13 +81,16 @@ const MembersList = () => {
                                             {member.Personal_Page.length !== 0 && <div><Link to={member.Personal_Page} target="_blank"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 256h64c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zm256-32H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg></Link></div>}
                                         </div>
                                     </span>
-                                    <div className="interest">
-                                        {member.Interests && member.Interests.split(";").map((interest) => {
-                                            return (
-                                                <span>{`${interest}`},&nbsp;</span>
-                                            )
-                                        })}
-                                    </div>
+                                    {member.Interests &&
+                                        <div className="interest">
+                                            <b>Interest:</b> &nbsp;
+                                            {member.Interests.split(";").map((interest) => {
+                                                return (
+                                                    <span>{`${interest}`},&nbsp;</span>
+                                                )
+                                            })}
+                                        </div>
+                                    }
                                 </div>
                             </div>
                         </Link> 
